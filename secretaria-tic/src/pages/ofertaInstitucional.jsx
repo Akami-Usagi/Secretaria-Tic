1d7aaf5f218592f52c5036c8e578ffc979b5d41f
Camilo Arango <akami.3ds@gmail.com>
2024-09-27 08:53:08 -0500

    imagenes oferta educativa convertidos en links

--- secretaria-tic/src/pages/ofertaInstitucional.jsx
+++ secretaria-tic/src/pages/ofertaInstitucional.jsx
@@ -97,12 +97,12 @@ export default function BugaAvanza(){
                 </Text>  
             </TextDiv>
             <CousesDiv>
-                <CourseImage src="/images/ofertaInstitucional/programacion.webp" alt="Programacion" />
-                <CourseImage src="/images/ofertaInstitucional/drones.webp" alt="Drones" />
-                <CourseImage src="/images/ofertaInstitucional/excel.webp" alt="Excel" />
-                <CourseImage src="/images/ofertaInstitucional/fotografia.webp" alt="Fotografia" />
-                <CourseImage src="/images/ofertaInstitucional/informatica.webp" alt="Informatica" />
-                <CourseImage src="/images/ofertaInstitucional/marketing.webp" alt="Marketing" />
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSdX8gU0rIkeehzPpVWq7UQMwIold_QZG8BgLc39p2g-OiHVOQ/viewform?usp=sf_link" target="blank"><CourseImage src="/images/ofertaInstitucional/programacion.webp" alt="Programacion" /></a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSeXkjfOGVgdSnAe23W2fr-up1ES_V9EXGUuryFMXsYkM4FSSA/viewform?usp=sf_link" target="blank"><CourseImage src="/images/ofertaInstitucional/drones.webp" alt="Drones" /></a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSdR-dLjJqKNaxSeIQzolltsP6oGqD8CnjGABPgAm62i8PtlXg/viewform?usp=sf_link" target="blank"><CourseImage src="/images/ofertaInstitucional/excel.webp" alt="Excel" /></a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSdczGhT_TDY6Gil6kSrmOceSSG1RyINJ2FbvXxEODl6JhEUjA/viewform?usp=sf_link" target="blank"><CourseImage src="/images/ofertaInstitucional/fotografia.webp" alt="Fotografia" /></a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSdAKXSDcR2EY3jKCT_40o57m9FIwgOaC30AUNvvm_hkHzjXPQ/viewform?usp=sf_link" target="blank"><CourseImage src="/images/ofertaInstitucional/informatica.webp" alt="Informatica" /></a>
+                <a href="https://docs.google.com/forms/d/e/1FAIpQLSfUCJmZmHSnE-FL72BYZ04x_15MmzvjpoaGbyFvu6dl7b_new/viewform?usp=sf_link" target="blank"><CourseImage src="/images/ofertaInstitucional/marketing.webp" alt="Marketing" /></a>
             </CousesDiv>
         </BugaDiv>
     )
